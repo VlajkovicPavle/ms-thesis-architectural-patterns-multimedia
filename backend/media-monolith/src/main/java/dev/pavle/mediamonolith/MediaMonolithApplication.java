0fd733d27455c514b4720ab71da628855a7a56d9
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2026-04-26 08:29:42 +0200

    init(media-monolith): setup spring boot project

---
+++ backend/media-monolith/src/main/java/dev/pavle/mediamonolith/MediaMonolithApplication.java
@@ -0,0 +1,13 @@
+package dev.pavle.mediamonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaMonolithApplication.class, args);
+    }
+
+}
---
+++ backend/media-monolith/src/test/java/dev/pavle/mediamonolith/MediaMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pavle.mediamonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
