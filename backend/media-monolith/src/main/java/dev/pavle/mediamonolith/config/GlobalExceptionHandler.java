5247c10865eb77d3fd69294cf2c93cc56d4960f8
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2026-04-26 20:32:28 +0200

    feat(media-monolith): add global exception handler

---
+++ backend/media-monolith/src/main/java/dev/pavle/mediamonolith/config/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package dev.pavle.mediamonolith.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import dev.pavle.mediamonolith.processing.exceptions.FileStorageException;
+import dev.pavle.mediamonolith.processing.exceptions.VideoProcessingException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(VideoProcessingException.class)
+  public ProblemDetail handleVideoProcessingException(VideoProcessingException ex) {
+    ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_CONTENT, ex.getMessage());
+    problem.setTitle("Video Processing Failed");
+    return problem;
+  }
+
+  @ExceptionHandler(FileStorageException.class)
+  public ProblemDetail handleFileStorageException(FileStorageException ex) {
+    ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    problem.setTitle("File Storage Failed");
+    return problem;
+  }
+}
