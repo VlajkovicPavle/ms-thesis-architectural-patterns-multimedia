4e3fd52f3f36e61101115a77e14ca05ee19e0cf4
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2026-05-03 08:04:42 +0200

    feat(media-monolith): add domain exception

--- backend/media-monolith/src/main/java/dev/pavle/mediamonolith/video/domain/port/EventDispatcherPort.java
+++
@@ -1,7 +0,0 @@
-package dev.pavle.mediamonolith.video.domain.port;
-
-import dev.pavle.mediamonolith.video.domain.event.CreateRenditionEvent;
-
-public interface EventDispatcherPort {
-  void dispatchRenditionCreation(CreateRenditionEvent event);
-}
